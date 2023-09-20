835a874f98d34dc0cdf594dbcdf6eb67d8981bc0
Thomas STEPHAN (shan0ar) <stephan.thomas.pro@gmail.com>
2023-09-20 11:48:18 +0200

    Update ssh_MailNotif.sh

--- ssh_MailNotif.sh
+++ ssh_MailNotif.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-expediteur="alert@TSN_bot.axians.com"
-destinataire="thomas.stephan@axians.com"
-objet="TSNdeb - SSH connection"
+source="alert@TSN_bot.com"
+destination="thomas.stephan@travail.com"
+object="TSNdeb - SSH connection"
 body="Nouvelle connexion SSH maintenant"
 
 if [ -n "${PAM_TYPE}" ] && [ "${PAM_TYPE}" = "open_session" ]; then
-    echo "${body}" | /usr/bin/mail -r "${expediteur}" -s "${objet}" "${destinataire}" -a "Content-Type: text/html"
+    echo "${body}" | /usr/bin/mail -r "${source}" -s "${object}" "${destination}" -a "Content-Type: text/html"
 fi
 exit 0
