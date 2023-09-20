b97c4a3357fb5e1c7aac5a8d8f332d428bad9add
Thomas STEPHAN (shan0ar) <stephan.thomas.pro@gmail.com>
2023-09-20 11:51:24 +0200

    Update ssh_MailNotif.sh

--- ssh_MailNotif.sh
+++ ssh_MailNotif.sh
@@ -9,3 +9,6 @@ if [ -n "${PAM_TYPE}" ] && [ "${PAM_TYPE}" = "open_session" ]; then
     echo "${body}" | /usr/bin/mail -r "${source}" -s "${object}" "${destination}" -a "Content-Type: text/html"
 fi
 exit 0
+
+
+#Vérifier que mailutils et postfix, pour tester que l'envoi de mail à lui seul fonctionne, commenter la ligne if et fi puis executer le script. Ensuite, visionner /var/log/mail.log des informations intéressantes peuvent s'y trouver si le mail ne s'envoi pas.
